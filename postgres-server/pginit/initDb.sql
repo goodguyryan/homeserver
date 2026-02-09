a1465bf453799f899676db49a3d6b99242a2a455
goodguyryan <flashsstudioz@gmail.com>
2026-02-09 17:07:14 +0800

    minor change

--- postgres-server/pginit/initDb.sql
+++ postgres-server/pginit/initDb.sql
@@ -2,9 +2,9 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE IF NOT EXISTS expenses (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    description TEXT,
+    description TEXT NOT NULL,
     amount NUMERIC(12, 2) NOT NULL CHECK (amount >= 0),
     day INTEGER NOT NULL CHECK (day >= 1 AND day <= 31),
     month INTEGER NOT NULL CHECK (month >= 1 AND month <= 12),
-    year INTEGER NOT NULL CHECK
+    year INTEGER NOT NULL
 );
