4563f4167d2a8308cb40261951909ed14f055ea9
goodguyryan <flashsstudioz@gmail.com>
2026-02-09 17:00:56 +0800

    Fixed SQL init command

--- postgres-server/pginit/initDb.sql
+++ postgres-server/pginit/initDb.sql
@@ -1,10 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE TABLE IF NOT EXISTS expenses {
+CREATE TABLE IF NOT EXISTS expenses (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     description TEXT,
     amount NUMERIC(12, 2) NOT NULL CHECK (amount >= 0),
     day INTEGER NOT NULL CHECK (day >= 1 AND day <= 31),
     month INTEGER NOT NULL CHECK (month >= 1 AND month <= 12),
     year INTEGER NOT NULL CHECK
-};
+);
