5127456601a3d99e019c4114ecc167ded984629f
goodguyryan <flashsstudioz@gmail.com>
2026-07-11 23:11:29 +0800

    update ansible_inventory.tf

--- cloudedge/infra/ansible_inventory.tf
+++ cloudedge/infra/ansible_inventory.tf
@@ -4,12 +4,11 @@ resource "local_file" "ansible_inventory" {
   content = yamlencode({
     all = {
       hosts = {
-        for key, droplet in digitalocean_droplet.vm : key => {
-          ansible_host                 = droplet.ipv4_address
+        vm = {
+          ansible_host                 = digitalocean_droplet.vm.ipv4_address
           ansible_user                 = "root"
           ansible_ssh_private_key_file = "~/.ssh/terraform"
         }
-        if key == "vm1"
       }
     }
   })
